990a7482498342416dcff9a40ad08829430d14d0
AliceX <58663120+Alice-Xzq@users.noreply.github.com>
2020-01-27 17:36:56 -0500

    update ElevatorNoEnc

--- ElevatorNoEnc.java
+++ ElevatorNoEnc.java
@@ -13,16 +13,10 @@ public class ElevatorNoEnc {
     int LIFT_COUNTS_PER_UPDOWN_EFFORT = 50;
     double LIFT_POWER = 1.0;
 
-    //Grabber Parameters
-    Servo grabber = null;
-    double GRABBER_CLOSED_POSITION = 0.0;
-    double GRABBER_OPENED_POSITION = 0.8 ;
-
     public void init(HardwareMap Map, Config config) {
         hwMap = Map;
 
         LIFT_POWER = config.getDouble("lift_power", 1.0);
-        LIFT_COUNTS_PER_UPDOWN_EFFORT = config.getInt("lift_counts_per_updown_effort", 50);
 
         GRABBER_CLOSED_POSITION = config.getDouble("grabber_closed_position",0.0);
         GRABBER_OPENED_POSITION = config.getDouble("grabber_open_position",0.8);
@@ -31,43 +25,20 @@ public class ElevatorNoEnc {
         grabber = hwMap.get(Servo.class, "grabber");
 
         elevator.setZeroPowerBehavior(ZeroPowerBehavior.FLOAT);
-        elevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         grabber.setDirection(Servo.Direction.FORWARD);
     }
 
-    public void setLiftZeroPosition() {
-        elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-    
     public void moveUp() {
-        int newTarget;
-        newTarget  = elevator.getCurrentPosition() + LIFT_COUNTS_PER_UPDOWN_EFFORT;
         elevator.setPower(LIFT_POWER);
-        elevator.setTargetPosition(newTarget);
-        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void moveDown() {
-        int newTarget;
-        newTarget  = elevator.getCurrentPosition() - LIFT_COUNTS_PER_UPDOWN_EFFORT;
-        elevator.setPower(LIFT_POWER);
-        elevator.setTargetPosition(newTarget);
-        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elevator.setPower(-LIFT_POWER);
     }
 
     public void stop() {
-        elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        elevator.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
         elevator.setPower(0.0);
-    }
-
-    public void closeGrabber() {
-        grabber.setPosition(GRABBER_CLOSED_POSITION);
-    }
-
-    public void openGrabber() {
-        grabber.setPosition(GRABBER_OPENED_POSITION);
     }
 }
